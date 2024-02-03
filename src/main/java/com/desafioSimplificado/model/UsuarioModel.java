de917d9cb457eddf3ef8110ebc450290f459a20a
Wandeson Rodrigues <wandesonrc@gmail.com>
2024-02-03 20:26:48 -0300

    feat: criando conexão com banco de dados e atualização da usuario model.

--- src/main/java/com/desafioSimplificado/model/UsuarioModel.java
+++ src/main/java/com/desafioSimplificado/model/UsuarioModel.java
@@ -1,24 +1,91 @@
 package com.desafioSimplificado.model;
 
 import jakarta.persistence.*;
-
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "Usuarios")
-public class UsuarioModel {
+@Table(name = "TB-USUARIOS")
+public class UsuarioModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nomeCompleto;
     @Column(unique = true)
     private String documento;
     @Column(unique = true)
     private String email;
-
     private String senha;
     private BigDecimal saldo;
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
+
+    public UsuarioModel() {
+    }
+
+    public UsuarioModel(Long id, String nomeCompleto, String documento, String email, String senha, BigDecimal saldo, TipoUsuario tipoUsuario) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.documento = documento;
+        this.email = email;
+        this.senha = senha;
+        this.saldo = saldo;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 }
